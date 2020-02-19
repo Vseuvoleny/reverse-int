d5146fede2ee6c6aa731b8ca17dcba557feeddcd
Arthur <Debruit63@gmai.com>
2020-02-19 22:12:43 +0400

    solve it reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let string = String(n);
+    if(string[0] === '-'){
+        string = string.slice(1)
+    }
+    return +Array.from(string)
+        .reverse()
+        .join("");
+};
